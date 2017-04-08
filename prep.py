6514aad1640b5266c860ad919ae0b47d0b9b395b
George Digkas <ma-los-sis@hotmail.com>
2017-04-08 15:36:28 +0200

    cdfERROR

--- prep.py
+++ prep.py
@@ -1,3 +1,19 @@
+import requests
+METRICS_ALL = 'accessors,new_technical_debt,blocker_violations,conditions_to_cover,new_it_conditions_to_cover,new_conditions_to_cover,bugs,burned_budget,business_value,class_complexity_distribution,classes,code_smells,comment_lines,commented_out_code_lines,comment_lines_density,comment_lines_data,complexity,class_complexity,file_complexity,function_complexity,complexity_in_classes,complexity_in_functions,branch_coverage,new_it_branch_coverage,new_branch_coverage,conditions_by_line,confirmed_issues,coverage,new_it_coverage,coverage_line_hits_data,new_coverage,covered_conditions_by_line,critical_violations,last_commit_date,directories,duplicated_blocks,new_duplicated_blocks,duplicated_files,duplicated_lines,duplicated_lines_density,new_duplicated_lines,new_duplicated_lines_density,duplications_data,effort_to_reach_maintainability_rating_a,executable_lines_data,false_positive_issues,file_complexity_distribution,files,function_complexity_distribution,functions,generated_lines,generated_ncloc,info_violations,violations,it_conditions_to_cover,it_branch_coverage,it_conditions_by_line,it_coverage,it_coverage_line_hits_data,it_covered_conditions_by_line,it_line_coverage,it_lines_to_cover,it_uncovered_conditions,it_uncovered_lines,line_coverage,new_it_line_coverage,new_line_coverage,lines,ncloc,ncloc_language_distribution,new_lines,lines_to_cover,new_it_lines_to_cover,new_lines_to_cover,sqale_rating,new_maintainability_rating,major_violations,minor_violations,ncloc_data,new_blocker_violations,new_bugs,new_code_smells,new_critical_violations,new_info_violations,new_violations,new_major_violations,new_minor_violations,new_vulnerabilities,open_issues,overall_conditions_to_cover,new_overall_conditions_to_cover,overall_branch_coverage,new_overall_branch_coverage,overall_conditions_by_line,overall_coverage,overall_coverage_line_hits_data,new_overall_coverage,overall_covered_conditions_by_line,overall_line_coverage,new_overall_line_coverage,overall_lines_to_cover,new_overall_lines_to_cover,overall_uncovered_conditions,new_overall_uncovered_conditions,overall_uncovered_lines,new_overall_uncovered_lines,quality_profiles,projects,public_api,public_documented_api_density,public_undocumented_api,quality_gate_details,alert_status,reliability_rating,new_reliability_rating,reliability_remediation_effort,new_reliability_remediation_effort,reopened_issues,security_rating,new_security_rating,security_remediation_effort,new_security_remediation_effort,skipped_tests,development_cost,statements,team_size,sqale_index,sqale_debt_ratio,new_sqale_debt_ratio,uncovered_conditions,new_it_uncovered_conditions,new_uncovered_conditions,uncovered_lines,new_it_uncovered_lines,new_uncovered_lines,test_data,test_execution_time,test_errors,test_failures,test_success_density,tests,vulnerabilities,wont_fix_issues'
+METRICS_NOTNULL = 'blocker_violations,bugs,classes,code_smells,comment_lines,comment_lines_density,complexity,class_complexity,file_complexity,function_complexity,complexity_in_classes,complexity_in_functions,confirmed_issues,critical_violations,last_commit_date,directories,duplicated_blocks,duplicated_files,duplicated_lines,duplicated_lines_density,effort_to_reach_maintainability_rating_a,false_positive_issues,file_complexity_distribution,files,function_complexity_distribution,functions,info_violations,violations,lines,ncloc,ncloc_language_distribution,sqale_rating,major_violations,minor_violations,open_issues,quality_profiles,quality_gate_details,alert_status,reliability_rating,reliability_remediation_effort,reopened_issues,security_rating,security_remediation_effort,development_cost,statements,sqale_index,sqale_debt_ratio,vulnerabilities,wont_fix_issues'
+METRICS_NOTNULL_NOTSTRING = 'blocker_violations,bugs,classes,code_smells,comment_lines,comment_lines_density,complexity,class_complexity,file_complexity,function_complexity,complexity_in_classes,complexity_in_functions,confirmed_issues,critical_violations,last_commit_date,directories,duplicated_blocks,duplicated_files,duplicated_lines,duplicated_lines_density,effort_to_reach_maintainability_rating_a,false_positive_issues,files,functions,info_violations,violations,lines,ncloc,major_violations,minor_violations,open_issues,reliability_remediation_effort,reopened_issues,security_remediation_effort,development_cost,statements,sqale_index,sqale_debt_ratio,vulnerabilities,wont_fix_issues'
+METRICS_NOTNULL_NOTSTRING_NOTLASTITEMVALUE = 'blocker_violations,bugs,classes,code_smells,comment_lines,comment_lines_density,complexity,class_complexity,file_complexity,function_complexity,confirmed_issues,critical_violations,last_commit_date,directories,duplicated_blocks,duplicated_files,duplicated_lines,duplicated_lines_density,effort_to_reach_maintainability_rating_a,false_positive_issues,files,functions,info_violations,violations,lines,ncloc,major_violations,minor_violations,open_issues,reliability_remediation_effort,reopened_issues,security_remediation_effort,development_cost,statements,sqale_index,sqale_debt_ratio,vulnerabilities,wont_fix_issues'
+METRICS_MTD = 'code_smells,bugs,classes,lines,ncloc,sqale_index,violations,blocker_violations,critical_violations,major_violations,minor_violations'
+METRICS_ECSA = 'complexity,class_complexity,file_complexity,function_complexity,comment_lines,comment_lines_density,duplicated_blocks,duplicated_files,duplicated_lines,duplicated_lines_density,violations,blocker_violations,critical_violations,major_violations,minor_violations,info_violations,open_issues,code_smells,sqale_index,sqale_debt_ratio,bugs,reliability_remediation_effort,vulnerabilities,security_remediation_effort,classes,directories,files,lines,ncloc,functions,statements'
+METRICS = METRICS_ECSA
+METRICS_LIST = METRICS.split(',')
+metrics_list_length = len(METRICS_LIST)
+
+#esolved=false
+resolutions = 'FALSE-POSITIVE,WONTFIX,FIXED,REMOVED'.split(',')
+severities = 'INFO,MINOR,MAJOR,CRITICAL,BLOCKER'.split(',')
+types = 'CODE_SMELL,BUG,VULNERABILITY'.split(',')
+
 #CLASSES
 
 HETZNER_IP = '176.9.137.234'
@@ -36,38 +52,49 @@ class GitHubProject:
 def get_repos():
     repos = []
     
-    repos.append(GitHubProject(160994,'apache','sling','Java'))
-    repos.append(GitHubProject(160999,'apache','zookeeper','Java'))
-    repos.append(GitHubProject(206338,'apache','directory-shared','Java'))
-    repos.append(GitHubProject(206350,'apache','cayenne','Java'))
-    repos.append(GitHubProject(206368,'apache','poi','Java'))
-    repos.append(GitHubProject(206378,'apache','commons-lang','Java'))
-    repos.append(GitHubProject(206483,'apache','maven','Java'))
-    repos.append(GitHubProject(318103,'apache','pdfbox','Java'))
-    repos.append(GitHubProject(322018,'apache','aries','Java'))
-    repos.append(GitHubProject(507775,'elastic','elasticsearch','Java'))
-    repos.append(GitHubProject(1553754,'eclipse','orion.server','Java'))
-    repos.append(GitHubProject(1553787,'eclipse','egit','Java'))
-    repos.append(GitHubProject(1553790,'eclipse','jgit','Java'))
-    repos.append(GitHubProject(1553793,'eclipse','linuxtools','Java'))
+    repos.append(GitHubProject(160994,'apache','sling','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(160999,'apache','zookeeper','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(206338,'apache','directory-shared','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(206350,'apache','cayenne','Java',sonarqube_ip='176.9.137.234'))
+    #repos.append(GitHubProject(206364,'apache','openjpa','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(206366,'apache','mina','Java',sonarqube_ip='129.125.5.41'))
+    repos.append(GitHubProject(206368,'apache','poi','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(206378,'apache','commons-lang','Java',sonarqube_ip='176.9.137.234'))
+    #repos.append(GitHubProject(206398,'apache','httpcore','Java',sonarqube_ip='145.97.155.98'))
+    #repos.append(GitHubProject(206452,'apache','roller','Java',sonarqube_ip='129.125.5.41'))
+    repos.append(GitHubProject(206483,'apache','maven','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(318103,'apache','pdfbox','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(322018,'apache','aries','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(507775,'elastic','elasticsearch','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(688352,'apache','jmeter','Java',sonarqube_ip='176.9.137.234'))
+    #repos.append(GitHubProject(832680,'apache','maven-surefire','Java',sonarqube_ip='129.125.5.41'))
+    repos.append(GitHubProject(892275,'square','retrofit','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(1148753,'spring-projects','spring-framework','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(1553754,'eclipse','orion.server','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(1553787,'eclipse','egit','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(1553790,'eclipse','jgit','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(1553793,'eclipse','linuxtools','Java',sonarqube_ip='176.9.137.234'))
     repos.append(GitHubProject(1936771,'google','truth','Java',sonarqube_ip='145.97.155.98'))
-    repos.append(GitHubProject(2108481,'spring-projects','spring-security-oauth','Java'))
-    repos.append(GitHubProject(2153096,'apache','sqoop','Java'))
-    repos.append(GitHubProject(2198510,'apache','flume','Java'))
-    repos.append(GitHubProject(2211243,'apache','kafka','Java'))
-    repos.append(GitHubProject(2282376,'apache','giraph','Java'))
-    repos.append(GitHubProject(2304277,'spring-projects','spring-roo','Java'))
-    repos.append(GitHubProject(2493904,'apache','tomcat','Java'))
-#     repos.append(GitHubProject(2740148,'apache','opennlp','Java',sonarqube_ip='129.125.5.41'))
+    repos.append(GitHubProject(2108481,'spring-projects','spring-security-oauth','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(2153096,'apache','sqoop','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(2198510,'apache','flume','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(2211243,'apache','kafka','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(2282376,'apache','giraph','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(2304277,'spring-projects','spring-roo','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(2442457,'apache','ambari','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(2493904,'apache','tomcat','Java',sonarqube_ip='176.9.137.234'))
+    #repos.append(GitHubProject(2740148,'apache','opennlp','Java',sonarqube_ip='129.125.5.41'))
     repos.append(GitHubProject(5152285,'square','okhttp','Java',sonarqube_ip='145.97.155.98'))
     repos.append(GitHubProject(10230369,'google','auto','Java',sonarqube_ip='145.97.155.98'))
-    repos.append(GitHubProject(15928650,'apache','struts','Java'))
-    repos.append(GitHubProject(16977479,'apache','cxf','Java'))
-    repos.append(GitHubProject(26389457,'apache','tomcat80','Java'))
-    repos.append(GitHubProject(27911088,'apache','nifi','Java'))
+    #repos.append(GitHubProject(13414105,'python','peps','Python',sonarqube_ip='145.97.155.98'))
+    repos.append(GitHubProject(15698466,'apache','jclouds','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(15928650,'apache','struts','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(16977479,'apache','cxf','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(26389457,'apache','tomcat80','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(27911088,'apache','nifi','Java',sonarqube_ip='176.9.137.234'))
     repos.append(GitHubProject(32848140,'apache','zeppelin','Java',sonarqube_ip='145.97.155.98'))
-#     repos.append(GitHubProject(41348333,'apache','apex-core','Java',sonarqube_ip='129.125.5.41'))
-#     repos.append(GitHubProject(41348334,'apache','apex-malhar','Java',sonarqube_ip='129.125.5.41'))
+    #repos.append(GitHubProject(41348333,'apache','apex-core','Java',sonarqube_ip='129.125.5.41'))
+    #repos.append(GitHubProject(41348334,'apache','apex-malhar','Java',sonarqube_ip='129.125.5.41'))
     
 #     repos.append(GitHubProject(106310,'junit-team','junit4','Java'))
 #     repos.append(GitHubProject(160994,'apache','sling','Java'))
@@ -261,5 +288,6 @@ def get_repos():
 	
 def get_repos1():
     repos = []
-    repos.append(GitHubProject(160994,'apache','sling','Java'))    
+    repos.append(GitHubProject(160994,'apache','sling','Java',sonarqube_ip='176.9.137.234'))
+    repos.append(GitHubProject(27911088,'apache','nifi','Java'))    
     return repos
